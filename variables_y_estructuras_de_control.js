8376a53c3aabff026cffa6f4cdef80e4f30595c9
Hugo Felipe <caceros.hugo@galileo.edu>
2022-02-07 21:33:47 -0600

    primer commit

---
+++ variables_y_estructuras_de_control.js
@@ -0,0 +1,30 @@
+/*
+Nombre: Hugo Francisco Felipe Caceros
+Carné: 14008201
+*/
+
+var mes = 10
+switch (mes){
+  case 12:
+  case 1:
+  case 2:
+    console.log = ("Es invierno")
+    break;
+  case 3:
+  case 4:
+  case 5:
+    console.log = ("Es primavera")
+    break;
+  case 6:
+  case 7:
+  case 8:
+    console.log = ("Es verano")
+    break;
+  case 9:
+  case 10:
+  case 11:
+    console.log = ("Es otoño")
+    break;
+}
+
+alert("Carnet: 14008201" + "\n" + "Nombre: Hugo Francisco Felipe Caceros")
